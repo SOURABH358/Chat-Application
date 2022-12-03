4704cc208554b5518abc42440effaa7afff6def7
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-12-03 14:01:37 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import LogIn from "./components/Login";
+import './App.css'
+export default function App(){
+    return (
+        <LogIn/>
+    )
+}
---
+++ src/components/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LogIn(){
+    return <h1>Login</h1>
+}
+
+export default LogIn;
