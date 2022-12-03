c19db2efa97efef29e726b36459db6968c1ccdf0
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-12-03 14:30:04 +0530

    added files

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-function LogIn(){
-    return <h1>Login</h1>
+function LogIn() {
+    return <section className="LogIn__section">
+        <div className="LogIn__container">
+            <h2>Log In</h2>
+            <form id = "LogIn__form" className="LogIn__form">
+                <p>Username</p>
+                <input id = "username" type="text" placeholder="Username" />
+                <p>Password</p>
+                <input type="password" />
+                <button className = "LogIn__btn" type="submit">Log In</button>
+            </form>
+            <p>Not Logged In? <a href="#">Sign Up</a></p>
+        </div>
+    </section>
 }
 
 export default LogIn;
