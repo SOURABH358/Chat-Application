c9e411dd8a912f0ad2d9f22ab0c1a78746f263f7
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-12-04 14:46:12 +0530

    added files

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyAqKNFezI2L8ImZJ--e9U4e6z6R0PNGgAM",
-  authDomain: "chat-application-ab1bd.firebaseapp.com",
-  projectId: "chat-application-ab1bd",
-  storageBucket: "chat-application-ab1bd.appspot.com",
-  messagingSenderId: "1029859322592",
-  appId: "1:1029859322592:web:a2ed1b3d92b9db85e1fe85"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
