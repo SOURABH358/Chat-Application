7e069dfb6b8ae8f9f63b4eab58f4792305339b08
Sourabh <75243401+SOURABH358@users.noreply.github.com>
2022-12-08 17:53:53 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LogIn from "./components/Login";
 import SignUp from "./components/SignUp";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Loading from "./components/Loading";
 import './App.scss'
 import { AppContext } from "./Context/context";
 export default function App(){
@@ -16,7 +17,8 @@ export default function App(){
     return (
         <Routes>
             <Route path="/" >
-                <Route index element={<ProtectedRoute><Home/></ProtectedRoute>}></Route>
+                {/* <Route index element={<ProtectedRoute><Home/></ProtectedRoute>}></Route> */}
+                <Route index element={<Loading/>}></Route>
                 <Route path="login" element={<LogIn/>}/>
                 <Route path="signup" element={<SignUp/>}/>
             </Route>
---
+++ src/components/Loading.js
@@ -0,0 +1,5 @@
+export default function Loading (){
+    return <section className="loading__section">
+        <img src="/assets/app_icons.png" alt="app_icon"/>
+    </section>
+}
